497334e90ab50e7fed41a1bc94df407c6fdea132
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-21 13:37:57 -0300

    feat: creates a vue component using data() function and props

---
+++ level-1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppComponent title="Hello World!" />
+</template>
+
+<script>
+import AppComponent from "./components/AppComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ level-1/src/components/AppComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Come back often"
+    };
+  },
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: #42b983;
+  margin: 0;
+}
+p{
+  color: #202020;
+  font-size: 32px;
+  margin: 4px 0 0;
+}
+</style>
